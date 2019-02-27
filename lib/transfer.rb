410a7d3b26bca876a1f4979854c106c5b853378f
Christoph Gruener <christophgruener@me.com>
2019-02-27 22:21:21 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -22,7 +22,7 @@ class Transfer
 
     else
       @status = "rejected"
-      "Transaction rejected. Please check your balance."
+      "Transaction rejected. Please check your account balance."
     end
   end
 
